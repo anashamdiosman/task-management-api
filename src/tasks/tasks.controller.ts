4200868f7bcc23aa1a51eb57f3d06850dd4bd4be
Anas Personal <anas-el-tom@live.com>
2026-02-11 09:48:26 +0400

    adding swagger

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,29 +15,35 @@ import {
   UpdateTaskDto,
 } from '../dtos/tasks.dto';
 import { Task } from 'src/entities/tasks.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all tasks' })
   getAllTasks(@Query() filterDto: FilterTasksDto): Promise<Task[]> {
     const { status, search } = filterDto;
     return this.tasksService.getAllTasks({ status, search });
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get Task by id' })
   getTaskById(@Param('id') id: string): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new task' })
   createTask(@Body() dto: CreateTaskDto): Promise<Task> {
     const { description, title } = dto;
     return this.tasksService.createTask({ description, title });
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a task' })
   updateTaskById(
     @Body() updateDto: UpdateTaskDto,
     @Param('id') id: string,
@@ -47,6 +53,7 @@ export class TasksController {
   }
 
   @Patch(':id/status')
+  @ApiOperation({ summary: "Update a task's status" })
   updateTaskStatusById(
     @Body() updateDto: UpdateTaskDto,
     @Param('id') id: string,
@@ -56,6 +63,7 @@ export class TasksController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a task' })
   deleteTaskById(@Param('id') id: string) {
     return this.tasksService.deleteTaskById(id);
   }
